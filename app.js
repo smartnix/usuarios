f82f99835de80cadd8b208991de3ed721a3b0994
Santiago <smartnix@hotmail.com>
2016-07-23 08:27:20 -0500

    rutas, metodos, creacion de base de datos urs

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var usuarios = require("./routes/usuarios");
 
 var app = express();
 
@@ -22,8 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+app.use("/usuarios",usuarios);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,1 +1,33 @@
 var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+    var nombre = req.query.nombre;
+    var apellido = req.query.apellido;
+
+    res.send({ msg: "metodo get", mombre:nombre+" "+apellido });
+});
+
+router.post("/", function(req, res, next) {
+    res.send({ msg: "metodo post" });
+});
+
+router.get("/:id", function(req, res, next) {
+    var id = req.params.id;
+    res.send({ msg: "metodo get", id: id });
+
+});//id nombre de la variable
+
+router.put("/:id", function(req, res, next) {
+    var id = req.params.id;
+    res.send({ msg: "metodo put", id: id });
+});
+
+router.delete("/:id", function(req, res, next) {
+    var id = req.params.id;
+    res.send({ msg: "metodo delete", id: id });
+});
+
+
+
+module.exports = router;
